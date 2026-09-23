8298b929c6194707e5c8a44bb5989f2b819e91d8
Diana Khmeliuk <dianakhmeluyk@gmail.com>
2026-09-23 09:05:19 +0300

    solution

---
+++ network.tf
@@ -0,0 +1,19 @@
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_network_interface" "main" {
+  for_each            = toset(local.network_interface_names)
+  name                = "${var.prefix}-nic-${each.value}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "prefix" {
+  default = "tfvmex"
+}
+
+variable "location" {
+  default = "West Europe"
+}
+
