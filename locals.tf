252e3db4f3d66407c434007ec85f89e0bfd0bbee
Diana Khmeliuk <dianakhmeluyk@gmail.com>
2026-09-23 09:13:10 +0300

    solution

--- locals.tf
+++ locals.tf
@@ -1,3 +1,16 @@
 locals {
   network_interface_names = ["nic1", "nic2", "nic3"]
+
+  security_rules = [
+    {
+      name                       = "ssh"
+      priority                   = 1000
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+    }
+]
 }
+
